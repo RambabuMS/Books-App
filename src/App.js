0669dd2257224b1b607b7483d3351a1f0b5ca93a
RambabuMS <raambabu96@gmail.com>
2022-02-22 09:57:28 +0530

    adjusted the position of drawer

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ export default function App() {
   ];
 
   const [state, setState] = useState({
-    top: false,
+    left: false,
   });
 
   const toggleDrawer = (anchor, open) => (event) => {
@@ -95,7 +95,7 @@ export default function App() {
         <div className="App">
           <AppBar position="static" color="success">
             <Toolbar>
-              {["top"].map((anchor) => (
+              {["left"].map((anchor) => (
                 <Fragment key={anchor}>
                   <Button color="inherit" onClick={toggleDrawer(anchor, true)}>
                     <MenuIcon />
