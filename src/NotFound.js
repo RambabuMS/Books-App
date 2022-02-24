a681267c8c40a738f8bcbbd62dc37dfd1dd1f0f8
Rambabu M <94340328+RambabuMS@users.noreply.github.com>
2022-02-24 21:03:45 +0530

    Updated link

--- src/NotFound.js
+++ src/NotFound.js
@@ -4,7 +4,7 @@ export function NotFound() {
       <h1 className="error-page">404 Page Not Found</h1>
       <img
         className="nfound"
-        src="https://cdn.dribbble.com/users/3480168/screenshots/10474202/gif-404-page.gif"
+        src="https://freefrontend.com/assets/img/html-funny-404-pages/CodePen-404-Page.gif"
         alt="Page Not Found"
         width="100%"
       />
